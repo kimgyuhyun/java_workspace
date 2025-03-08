baef9ac54355f9909f03f180816915c35147080e
Kimgyuhyun <kgh980605@gmail.com>
2025-03-09 02:22:16 +0900

    test: validate sum calculation with TestSumcalculaotr, ensure region name extraction from zoo object with TestGetRagionNameFromZooObject, check initialization by region name with TestInitializeByRegionName

---
+++ Traffic signal/src/com/example/test/RegionTest.java
@@ -0,0 +1,32 @@
+package com.example.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+//package com.example.test;
+//
+//public class Region {
+//	public String region;
+//	
+//	public Region(String region) {
+//		this.region = region; //Region region = new Region("인천");
+//	}
+//	
+//	public String getRegion() {
+//		return this.region;
+//	}
+//}
+////A = 10;
+////return A;
+
+class RegionTest {
+
+	@Test
+	void testInitializeByRegionName() {
+		Region incheonRegion = new Region("인천");
+		Region seoulRegion = new Region("서울");
+		assertEquals("서울", seoulRegion.getRegion());
+		assertEquals("인천", incheonRegion.getRegion());
+	}	
+
+}
---
+++ Traffic signal/src/com/example/test/ZooTest.java
@@ -0,0 +1,24 @@
+package com.example.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ZooTest {
+
+	@Test
+	void TestSumCalculator() {
+		// 덧셈 테스트
+		assertEquals(3, Zoo.Calculator(1, 2));
+	}
+	
+	@Test
+	void TestGetRegionNameFromZooObject() {
+		Region incheonRegion = new Region("인천");
+		Region seoulRegion = new Region("서울");
+		Zoo incheonZoo = new Zoo(incheonRegion);
+		Zoo seoulZoo = new Zoo(seoulRegion);
+		assertEquals("인천", incheonZoo.getRegionName());
+		assertEquals("서울", seoulZoo.getRegionName());
+	}
+}
