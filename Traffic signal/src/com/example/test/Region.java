714f404632f5d2ae2cd7c15fb01c0661e18300d5
Kimgyuhyun <kgh980605@gmail.com>
2025-03-09 02:27:58 +0900

    feat: creat zoo class with region instance, animal addtion/removal, and list methods, include Animal class with getter method

---
+++ Traffic signal/src/com/example/test/Region.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+public class Region {
+	public String region;
+	
+	public Region(String region) {
+		this.region = region; //Region region = new Region("인천");
+	}
+	
+	public String getRegion() {
+		return this.region;
+	}
+}
+//A = 10;
+//return A;
