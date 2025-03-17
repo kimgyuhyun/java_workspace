f4a93b1931b87e34ff1cfe5fd8bb73ee855a9441
Kimgyuhyun <kgh980605@gmail.com>
2025-03-17 18:55:49 +0900

    packge modifacation

--- 소스/chap08/src/sec01/exam06/TestRemoteControl.java
+++ 소스/chap08/src/sec01/exam06/TestRemoteControl.java
@@ -1,12 +1,12 @@
-package sec01.exam06;
-//인터페이스
-public interface TestRemoteControl {
-	//상수
-	int MAX_VOLUME = 10;
-	int MIN_VOLUME = 0;
-	
-	//추상 메소드
-	void turnOn();
-	void turnOff();
-	void setVolume(int volume);
-}
+//package sec01.exam06;
+////인터페이스
+//public interface TestRemoteControl {
+//	//상수
+//	int MAX_VOLUME = 10;
+//	int MIN_VOLUME = 0;
+//
+//	//추상 메소드
+//	void turnOn();
+//	void turnOff();
+//	void setVolume(int volume);
+//}
